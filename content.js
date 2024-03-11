76a3bffe1a689dc78474f67326e66043852fc85f
jwkelsch <78615417+jwkelsch@users.noreply.github.com>
2024-03-11 18:35:50 -0500

    update

--- content.js
+++ content.js
@@ -1,2 +1,19 @@
-var test = document.getElementById("education");
-test.textContent = "testeducaton"
+//darkmode toggle button
+
+darkmodeButton = document.getElementById("darkmodetoggle");
+links = document.getElementById("links");
+var toggle = 0;
+
+darkmodeButton.addEventListener("click", function(){
+    if (toggle==0){
+        document.body.style.background = "#121212";
+        document.body.style.color = "rgba(91, 108, 124, 0.842)";
+        //links.style.color = "lightsteelblue"
+        toggle++;
+    }
+    else{
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+        toggle--;
+    }
+});
